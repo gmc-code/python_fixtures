2fabcea3093aea5e0433f5317ee975ffa1b9c33f
gmc-code <gmccarthy@tpg.com.au>
2023-12-18 16:15:50 +1100

    up

--- docs/conf.py
+++ docs/conf.py
@@ -29,3 +29,17 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+html_theme_options = {
+    'logo_only': False,
+    'display_version': False,  # False so doc version not shown
+    'prev_next_buttons_location': 'both',  # Can be bottom, top, both , or None
+    'style_external_links': True,  # True to Add an icon next to external links
+    'style_nav_header_background': 'linear-gradientlinear-gradient(to right, blueviolet 15%, limegreen 50%, royalblue 80%)',  # blue
+    # Toc options
+    'collapse_navigation': False,  # False so nav entries have the [+] icons
+    'sticky_navigation': False,  # False so the nav does not scroll
+    'navigation_depth': 4,  # -1 for no limit
+    'includehidden': True,  # displays toctree that are hidden
+    'titles_only': False  # False so page subheadings are in the nav.
+}
