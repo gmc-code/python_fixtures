4a2f3a647f8247200b3efa2668bd4e48e5ed8883
gmc-code <gmccarthy@tpg.com.au>
2023-12-18 16:13:37 +1100

    up

--- docs/conf.py
+++ docs/conf.py
@@ -9,11 +9,14 @@
 project = 'Python_fixtures'
 copyright = '2023, GMC'
 author = 'GMC'
+import sphinx_rtd_theme
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx_rtd_theme'
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -23,5 +26,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
